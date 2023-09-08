07a6c028d97e3cb69d3c5320c4fbeddbb25782a9
Junhao Shen <sjhxbug1024@gmail.com>
2023-09-08 14:39:47 -0500

    add forgot password

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
+import UserProfile from './pages/user/Profile';
 import RegistrationPage from './pages/login/RegistrationPage';
 import Home from './pages/Home';
 import './stylesheets/App.css';
@@ -12,6 +13,7 @@ const App: React.FC = () => {
           <Route path="/login" element={<LoginPage/>}/>
           <Route path="/register" element={<RegistrationPage/>}/>
           <Route path="/home" element={<Home/>}/>
+          <Route path="/profile" element={<UserProfile />} />
           <Route path="*" element={<LoginPage/>} />
         </Routes>
       </Router>
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,14 +1,15 @@
 import React, { useState, FormEvent } from 'react';
 import {
   Box,
-  Button,
+  Grid,
+  Link,
   Container,
   FormHelperText,
   TextField,
   Typography,
 } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import AuthService from "../../services/auth";
 
 const LoginPage: React.FC = () => {
@@ -55,7 +56,18 @@ const LoginPage: React.FC = () => {
           <TextField label="Username" margin="normal" required fullWidth autoComplete="email" onChange={(e) => setUsername(e.target.value)} value={username} autoFocus />
           <TextField label="Password" margin="normal" required fullWidth type="password" onChange={(e) => setPassword(e.target.value)} value={password} autoComplete="current-password" />
           <LoadingButton type="submit" variant="contained" loading={loading} sx={{ mt: 4, mb: 3 }}>Login</LoadingButton>
-          <Button component={RouterLink} variant="text" to='/register' sx={{ mt: 4, mb: 3 }} >Register</Button>
+          <Grid container>
+              <Grid item xs>
+                <Link href="/forgot-password" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/register" variant="body2">
+                  {"Register"}
+                </Link>
+              </Grid>
+            </Grid>
           <FormHelperText>{message}</FormHelperText>
         </Box>
       </Box>
